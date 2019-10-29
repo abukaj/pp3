f6596dca3d1f7edd6cdd3fb6662fd26c0c2cbe9e
Jakub M. Dzik <abukaj@users.noreply.github.com>
2019-10-29 23:02:18 +0100

    Pierwsza kartkowka (bez tresci zadania)

---
+++ zajecia_5/README.md
@@ -0,0 +1,34 @@
+Zajęcia 4
+---------
+
+## Kartkówka
+
+Uzupełnij szablon "kartkowka1.py" wpisując swój numer albumu oraz
+definiując we wskazanym miejscu funkcję `(x)` zwracającą
+liczby `x` (definicja na tablicy).
+
+
+### Pobranie szablonu rozwiązania
+
+```bash
+$ git pull
+```
+
+
+### Podpowiedź
+
+Zdefiniowana funkcja powinna przejść następujące testy:
+```python
+assert == 0
+assert == 1
+assert == 2
+```
+
+
+### Przesłanie rozwiązania
+
+Plik "kartkowka1.py" zawierający rozwiązanie zadania należy przesłać
+jako załącznik wiadomości e-mail zatytułowanej "Kartkówka 1" na dwa
+adresy e-mail:
+ - kartkowka20191030@amix.org.pl
+ - j.kowalski@nencki.gov.pl
---
+++ zajecia_5/kartkowka1.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+# Kartkowka nr 1
+# Nr albumu:
+
+# Zdefiniuj ponizej (pomiedzy znacznikami) funkcje (x)
+# zwracajaca liczby x
+
+### POCZATEK DEFINICJI
+
+
+
+
+
+### KONIEC DEFINICJI
+
+# W razie potrzeby znaczniki mozna bardziej rozsunac, ale te funkcje
+# mozna poprawnie zdefiniowac w 5 linijkach
+
+### TESTY
+
+# Aby przekonac sie, czy funkcja zostala zdefiniowana poprawnie,
+# mozesz odkomentowac ponizsze linijki z testami i uruchomic program.
+# Zachecam do programowania napedzanego testami - tzn. stopniowego
+# odkomentowywania kolejnych testow i takiego poprawiania definicji
+# funkcji, zeby odkomentowane testy nie konczyly sie bledem.
+
+if __name__ == '__main__':
+    # assert == 0
+    # assert == 1
+    # assert == 2
