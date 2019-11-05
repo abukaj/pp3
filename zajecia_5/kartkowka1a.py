4a6140e6046ad651634bfa8f722e83fc0eeaa238
Jakub M. Dzik <abukaj@users.noreply.github.com>
2019-11-05 21:02:54 +0100

    dodatkowa wersja kartkowki

---
+++ zajecia_5/kartkowka1a.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+# Kartkowka nr 1
+# Nr albumu:
+
+# Zdefiniuj ponizej (pomiedzy znacznikami) funkcje f(x)
+# zwracajaca wartosc funkcji zdefiniowanej matematycznie na tablicy.
+
+### POCZATEK DEFINICJI
+
+
+
+
+
+### KONIEC DEFINICJI
+
+# W razie potrzeby znaczniki mozna bardziej rozsunac, ale te funkcje
+# mozna poprawnie zdefiniowac w 5 linijkach
+
+### TESTY
+
+# Aby przekonac sie, czy funkcja zostala zdefiniowana poprawnie,
+# mozesz odkomentowac ponizsze linijki z testami i uruchomic program.
+# Zachecam do programowania napedzanego testami - tzn. stopniowego
+# odkomentowywania kolejnych testow i takiego poprawiania definicji
+# funkcji, zeby odkomentowane testy nie konczyly sie bledem.
+
+if __name__ == '__main__':
+    # assert f(0) == 0
+    # assert f(-2) == -2
+    # assert f(1) == 2
+
